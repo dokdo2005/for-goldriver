c4ab605cbaa22c3576e8a2de1ec52010117fe8c1
Gary Lee <dokdo2005@gmail.com>
2022-05-14 20:53:55 +0900

    feat: 마우스 우클릭, 드래그 방지 추가

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import "./css/index.css";
 
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
