262104ce3111bd897e4b1e9e06a897880df64f7d
Gary Lee <dokdo2005@gmail.com>
2022-05-15 01:35:36 +0900

    mod: JSON 파일 위치 변경

--- src/components/messageList.jsx
+++ src/components/messageList.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
+import axios from "axios";
 import "../css/font.css";
 import "../css/messageList.css";
-import { messages } from "../json/messageData.json";
+// import { messageList } from "../json/messageData.json";
 
 function MessageList() {
+  const [messageList, setMsgList] = useState([]);
+
+  useEffect(() => {
+    const getMessageList = async () => {
+      const list = await axios.get(
+        `${import.meta.env.VITE_API_URI}/messageData.json`
+      );
+      setMsgList(list.data.messages);
+    };
+    getMessageList();
+    return setMsgList([]);
+  }, []);
+
   return (
     <div className="msgListContainer">
-      {messages.map((message) => (
+      {messageList.map((message) => (
         <div className="msgListEntry" key={message.id}>
           <div className="msgImage">
             {message.image ? <img src={message.image} width={"100%"} /> : null}
