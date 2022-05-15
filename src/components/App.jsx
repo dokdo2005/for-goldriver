97a611158e33a7935ada0ff2f87ff295edd5cc62
Gary Lee <dokdo2005@gmail.com>
2022-05-15 15:15:20 +0900

    feat: 음악 재생 버튼 추가

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import "../css/App.css";
+import MusicButton from "./musicButton";
 import Header from "./header";
 import MessageList from "./messageList";
 
 function App() {
   return (
     <div className="App">
+      <MusicButton />
       <Header />
       <MessageList />
     </div>
---
+++ src/components/musicButton.jsx
@@ -0,0 +1,34 @@
+import { React, useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh, faVolumeXmark } from "@fortawesome/free-solid-svg-icons";
+import BgMusic from "../music/journeys_end.mp3";
+import "../css/musicButton.css";
+
+function MusicButton() {
+  const [audio] = useState(new Audio(BgMusic));
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  const togglePlay = () => {
+    playing ? setPlaying(false) : setPlaying(true);
+  };
+
+  audio.loop = true;
+
+  return (
+    <div className="musicBtnContainer">
+      <button className="musicPlayButton" onClick={togglePlay}>
+        {playing ? (
+          <FontAwesomeIcon icon={faVolumeHigh} />
+        ) : (
+          <FontAwesomeIcon icon={faVolumeXmark} />
+        )}
+      </button>
+    </div>
+  );
+}
+
+export default MusicButton;
