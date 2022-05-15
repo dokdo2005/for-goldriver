3104a233fd3ffcf4ffef9907e10b4a2f37e68084
Gary Lee <dokdo2005@gmail.com>
2022-05-15 18:18:20 +0900

    mod: 헤더 문구 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -7,10 +7,18 @@ function Header() {
   return (
     <div className="headerContainer">
       <div className="headerImage">
-        <img src={GoldRiver} width={"50%"} />
+        <img src={GoldRiver} />
       </div>
       <div className="headerText">
         <div className="headerTitle">Good Bye, My Dear</div>
+        <div className="headerQuote">
+          <blockquote>
+            <div>꿈 꾸세요.</div>
+            <div>여러분들 다 하실 수 있습니다.</div>
+            <div>여러분의 가치를 항상 높여 잡아주셨으면 좋겠어요.</div>
+            <div className="quoteFooter">2022.01.28 깜짝 라이브에서</div>
+          </blockquote>
+        </div>
         <div className="headerDesc">
           <div>우리에게 늘 꿈을 꾸게 해준 당신</div>
           <div>감사합니다</div>
