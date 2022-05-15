b33d44e189f19c90fc12c8d7caaff40ae776fa58
Gary Lee <dokdo2005@gmail.com>
2022-05-15 19:39:36 +0900

    feat: redux store 추가

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 import "./css/index.css";
 
 document.addEventListener("contextmenu", (event) => event.preventDefault());
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
