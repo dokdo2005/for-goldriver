46407df31449081d706336bde20ac590622e72bd
Gary Lee <dokdo2005@gmail.com>
2022-05-31 02:29:12 +0900

    feat: 맨 위로 가기 버튼 추가

--- src/components/messageScreen.jsx
+++ src/components/messageScreen.jsx
@@ -1,14 +1,25 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "../css/messageScreen.css";
 import MusicButton from "./musicButton";
 import Header from "./header";
 import MessageList from "./messageList";
 
 function MessageScreen() {
+  const scrollTop = () => {
+    if (window.scrollY !== 0) {
+      window.scroll(0, 0);
+    }
+  };
+
   return (
     <div className="messageScreen">
       <MusicButton />
       <Header />
       <MessageList />
+      <button className="goTopButton">
+        <FontAwesomeIcon icon={faArrowUp} onClick={() => scrollTop()} />
+      </button>
     </div>
   );
 }
