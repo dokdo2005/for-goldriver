22044221b0e3e3e05ca28d13a7c0830a378cc63f
Gary Lee <dokdo2005@gmail.com>
2022-06-01 13:08:01 +0900

    mod: 헤더 문구 변경

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -12,8 +12,7 @@ function Header() {
       <div className="headerText">
         <div className="headerTitle">Good Bye, My Dear</div>
         <div className="headerDesc">
-          <div>우리에게 늘 꿈을 꾸게 해준 당신</div>
-          <div>감사합니다</div>
+          <div>미처 담지 못한 이야기들</div>
         </div>
         <div className="creditText">Made by 김뷰엘 with ❤️</div>
       </div>
