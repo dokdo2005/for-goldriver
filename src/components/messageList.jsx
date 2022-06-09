c4eb1a4ee0298ca0a3782a08d3973f8c3d0f1559
Gary Lee <dokdo2005@gmail.com>
2022-06-09 21:06:20 +0900

    mod: Mirag님 만화 추가

--- src/components/messageList.jsx
+++ src/components/messageList.jsx
@@ -47,7 +47,25 @@ function MessageList() {
           <div className="msgContent">{message.content}</div>
         </div>
       ))}
-      <div className="itemHidden"></div>
+      <div className="msgListEntry">
+        <a
+          href="https://www.inven.co.kr/board/lostark/4811/4966013"
+          target="_blank"
+        >
+          <div
+            className="msgImage"
+            style={{
+              backgroundImage: `url(${"https://for-goldriver-files.s3.ap-northeast-2.amazonaws.com/images/image-mirag.jpg"})`,
+            }}
+          >
+            <div className="imageEnlarge">
+              <FontAwesomeIcon icon={faMagnifyingGlassPlus} />
+            </div>
+          </div>
+        </a>
+        <div className="msgWriter">Mirag</div>
+        <div className="msgContent">[만화] 엠마랑 이고바바</div>
+      </div>
     </div>
   );
 }
